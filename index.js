101a4760d254d296d626d92e9dff140d65e2da46
Matwy <mattiabasket2004@gmail.com>
2022-10-06 15:31:21 +0200

    ping for categories

--- index.js
+++ index.js
@@ -2,34 +2,32 @@ var map = L.map('map', {
     center: [51.505, -0.09],
     zoom: 2
 });
+var shopIcon = L.icon({
+    iconUrl: 'store-2017.svg',
+
+    iconSize: [40, 40], // size of the icon
+});
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-async function createShops() {
+async function getCategories() {
     //get categories
     const response = await fetch('https://dummyjson.com/products/categories')
-    const categories = await JSON.stringify(response.json());
+    const categories = await response.json()
 
-    let shops = []
-    for (category in categories) {
-        let response = await fetch(`https://dummyjson.com/products/category/${category}`)
-        const products = await JSON.stringify(response.json());
-        let shop = {
-            category: category,
-            products: products
-        }
-        shops.push(shop)
-    }
-    return shops
+    return categories
 }
 async function addShops() {
-    let shops = await createShops()
+    let categories = await getCategories()
     for (let i = 0; i < shopPosition.length; i++) {
         let position = L.latLng(shopPosition[i][0], shopPosition[i][1])
-        let marker = L.marker(position).addTo(map)
-        marker.bindPopup(`<b>${shops[i].category}</b><br>I am a popup.`);
+        let marker = L.marker(position, {icon:shopIcon}).addTo(map)
+        let category = categories[i].toUpperCase()
+        marker.bindPopup(`<b>${category}</b><br>I am a popup.`);
     }
+    
 }
 addShops()
