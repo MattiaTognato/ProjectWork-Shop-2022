4e840f2f7a08d8d8ca298296d7d34562a5e30d11
DonMarselo <90642035+DonMarselo@users.noreply.github.com>
2022-10-07 09:32:42 +0200

    Login
    Sas

---
+++ script_login.js
@@ -0,0 +1,9 @@
+user = document.getElementById("");
+password = document.getElementById("");
+
+async function getResponse() {
+	const response = await fetch('https://dummyjson.com/users/filter?key=hair.color&value=Brown')
+		{
+
+		}
+}
