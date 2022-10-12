99eacf9d7e9887fe945ab4a972c2d2000c11cc0d
Matwy <mattiabasket2004@gmail.com>
2022-10-12 18:28:10 +0200

    prodotti pagina laterale

--- src/home.js
+++ src/home.js
@@ -13,7 +13,7 @@ var map = L.map('map', {
     maxBounds: mapBounds
 })
 var shopIcon = L.icon({
-    iconUrl: '../assets/store-2017.svg',
+    iconUrl: '../assets/store-2017.png',
     iconSize: [40, 40], // size of the icon
 });
 
@@ -27,7 +27,7 @@ async function getCategories() {
 
     return categories
 }
-async function addShops() {
+(async function addShops() {
     // create shops on the map
     // and then bind the popUp to all the shop
     
@@ -42,10 +42,31 @@ async function addShops() {
 
         //create popUp
         let popUpHTML = `<p class="text-center text-sm font-extrabold bg-transparent text-black" aria-current="page">${categories[i].toUpperCase()}</p>
-        <a href="../pages/shop-page.html?category=${categories[i]}" class="bg-fuchsia-500 hover:bg-fuchsia-600 hover:text-white visited:text-white text-white font-bold py-2 px-4 rounded-full">Buy Here</a>`
+        <a onclick="checkoutHandler(false)" class="bg-fuchsia-500 hover:bg-fuchsia-600 hover:text-white visited:text-white text-white font-bold py-2 px-4 rounded-full">Buy Here</a>`
         //bind it to the marker
         marker.bindPopup(popUpHTML, {minWidth : 90, offset: L.point(0, -13)})
     }
     
-}
-addShops()
+})()
+
+
+let checkout = document.getElementById("checkout");
+let checdiv = document.getElementById("chec-div");
+let flag3 = true;
+const checkoutHandler = () => {
+    if (!flag3) {
+        checkout.classList.add("translate-x-full");
+        checkout.classList.remove("translate-x-0");
+        setTimeout(function () {
+            checdiv.classList.add("hidden");
+        }, 300);
+        flag3 = true;
+    } else {
+        setTimeout(function () {
+            checkout.classList.remove("translate-x-full");
+            checkout.classList.add("translate-x-0");
+        }, 200);
+        checdiv.classList.remove("hidden");
+        flag3 = false;
+    }
+};
